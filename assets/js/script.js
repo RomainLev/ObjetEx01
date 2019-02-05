ef94db8c051709bf18e7a88e52e6615aaeb17cfc
Romain Levasseur <17664@csmrouen.net>
2019-02-05 12:04:18 +0100

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
 var voiture = new Object;
 voiture.masse = 800;
-voiture.vitesse = 200;
-voiture.marque = "Renault";
+voiture.vitesse = 56;
 voiture.modèle = "Twingo";
 
 var energieCynétique = (voiture.masse/2 * voiture.vitesse**2);
+document.write(energieCynétique);
 
-alert (energieCynétique) ;
+
+//Correction
+voiture1 = {} ;
+voiture1.masse = 1200;
+voiture1.vitesse = 22;
+voiture1.marque = "Tesla";
+voiture1.modele = "P100D";
+voiture1.calculerEnergieCinetique = function() {
+  if (voiture1.masse <= 0){
+    return false;
+  }
+
+  else {
+    return 0.5 * voiture1.masse * (voiture1.vitesse ** 2);
+  }
+};
+voiture.masse = 1600;
+alert(voiture1.calculerEnergieCinetique());
